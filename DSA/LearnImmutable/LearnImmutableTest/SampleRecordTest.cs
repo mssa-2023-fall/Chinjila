724203c7c958608cc9ecce9f31351b65b6027e9a
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-09-05 11:00:13 -0500

    Added Unit Tests to verify MS Doc's description.

---
+++ DSA/LearnImmutable/LearnImmutableTest/SampleRecordTest.cs
@@ -0,0 +1,86 @@
+namespace LearnImmutableTest
+{
+    [TestClass]
+    public class SampleRecordTest
+    {
+        [TestMethod]
+        public void TestRecordTypeEqualityWithPositionParameters()
+        {
+            //arrange
+            SampleRecord record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
+            SampleRecord record2 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
+
+            //assert
+            Assert.AreEqual(record1, record2);
+            Assert.AreNotSame(record1, record2);
+        }
+        [TestMethod]
+        public void TestRecordTypeInEqualityWithPositionParameters()
+        {
+            //arrange
+            SampleRecord record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
+            SampleRecord record2 = new SampleRecord(ParamString: "Test", ParamInt: 2, ParamDate: new DateTime(2023, 9, 5));
+
+            //assert
+            Assert.AreNotEqual(record1, record2);
+            Assert.AreNotSame(record1, record2);
+        }
+        [TestMethod]
+        public void TestRecordTypeSamenessWithPositionParameters()
+        {
+            //arrange
+            SampleRecord record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
+            SampleRecord record2 = record1;
+
+            //assert
+            Assert.AreEqual(record1, record2);
+            Assert.AreSame(record1, record2);
+        }
+
+        [TestMethod]
+        public void TestRecordTypeAutoImplementedProperties()
+        {
+            //Arrange
+            SampleRecord record1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
+
+            //Assert
+            Assert.AreEqual("Test", record1.ParamString);
+            Assert.AreEqual(1, record1.ParamInt);
+            Assert.AreEqual(new DateTime(2023, 9, 5), record1.ParamDate);
+
+        }
+
+        [TestMethod]
+        public void TestRecordTypeCanHaveMutableProperties()
+        {
+            //Arrange
+            string expected = "NewString";
+            SampleRecord record1 =
+                new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5)) { MutableProperty = "InitialString" };
+            //Act
+            record1.MutableProperty = expected;
+            //Assert
+            Assert.AreEqual(record1.MutableProperty, expected);
+        }
+
+        [TestMethod]
+        public void TestRecordTypeHaveDestructMethodWithOutParam()
+        {
+            //Arrange
+            SampleRecord record1 =
+                new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5)) { MutableProperty = "InitialString" };
+            string outString = String.Empty;
+            int outInt = 0;
+            DateTime outDateTime = new DateTime();
+
+            //Act
+            record1.Deconstruct(out outString, out outInt, out outDateTime);
+
+
+            //Assert
+            Assert.AreEqual(outString, "Test");
+            Assert.AreEqual(outInt, 1);
+            Assert.AreEqual(outDateTime, new DateTime(2023, 9, 5));
+        }
+    }
+}
