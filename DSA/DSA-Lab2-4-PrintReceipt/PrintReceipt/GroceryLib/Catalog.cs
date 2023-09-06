e940826a26b9b4c109bf26d96331a2c12ceb0357
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-09-06 10:51:20 -0500

    Complete Lab2-Ex4 Receipt Printer, ToDo-Make Unit Test

---
+++ DSA/DSA-Lab2-4-PrintReceipt/PrintReceipt/GroceryLib/Catalog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryLib
+{
+    public class Catalog
+    {
+        public static Product[] Products = {
+            new Product(1,"Apple",1.99M),
+            new Product(2,"Banana",2.49M),
+            new Product(3,"Orange",0.99M),
+            new Product(4,"Peach",1.29M),
+            new Product(5,"Watermelon",3.99M)
+        };
+
+        public static StringBuilder PrintMenu() {
+            StringBuilder sb = new StringBuilder();
+            return PrintMenu(sb);
+        }
+        public static StringBuilder PrintMenu(StringBuilder sb)
+        {
+            foreach (Product p in Products)
+            {
+                sb.AppendLine($"[{p.Id}] {p.Name,10} {p.Price:c2}");
+            }
+            return sb;
+        }
+    }
+}
---
+++ DSA/DSA-Lab2-4-PrintReceipt/PrintReceipt/GroceryLib/Product.cs
@@ -0,0 +1,6 @@
+﻿namespace GroceryLib
+{
+    public record class Product(int Id,string Name, decimal Price);
+    
+   
+}
