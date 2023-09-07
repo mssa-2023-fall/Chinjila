f396a7283bf94bc480d611190f537d0fbef17194
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-09-07 10:36:21 -0500

    Started with Lab4 Interface definition for LinkedList

---
+++ DSA/DSA-Lab3/Lesson3/BubbleSort/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BubbleSort;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<MSSASorts>();
+    }
+}
