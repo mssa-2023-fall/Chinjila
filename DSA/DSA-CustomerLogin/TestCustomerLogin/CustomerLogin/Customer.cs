584eb9cf70c4b7139297371c64c54a33d33e6f65
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-09-13 13:50:56 -0500

    Customer Login and test done

--- DSA/DSA-CustomerLogin/TestCustomerLogin/CustomerLogin/Customer.cs
+++ DSA/DSA-CustomerLogin/TestCustomerLogin/CustomerLogin/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,17 +9,25 @@ namespace CustomerLogin
 {
     public class Customer
     {
-        public Customer(string name,string email, byte[] passwordHash, byte[] creditCardHash)
+        private CryptoHelper _cryptoHelper = new();
+        public Customer(string name,string email, string password, string creditCard)
         {
+            Salt = RandomNumberGenerator.GetBytes(64);
             Name = name;
             Email = email;
-            PasswordHash = passwordHash;
-            CreditCardHash = creditCardHash;
+            PasswordHash = _cryptoHelper.ComputeHash(password, Salt);
+            CreditCardHash = _cryptoHelper.EncryptCreditCard(password,creditCard).Result;
         }
 
+        public byte[] Salt;
         public string Name { get; }
         public string Email { get; }
         public byte[] PasswordHash { get; }
         public byte[] CreditCardHash { get; }
+        public string ReadCreditCardPlainText(string password) {
+            return _cryptoHelper.DecryptCreditCard(password, this.CreditCardHash).Result;
+        }
     }
+
+
 }
