6adb37518d6750fdcdccddddd1882266e3727184
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-09-13 12:29:07 -0500

    Skeletal code for starting CustomerLogin

---
+++ DSA/DSA-CustomerLogin/TestCustomerLogin/CustomerLogin/CryptoHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerLogin
+{
+    public class CryptoHelper
+    {
+        public byte[] ComputeHash(string input, byte[] salt) { 
+        }
+
+        public bool VerifyHash(string clearPasswor,byte[] passwordHash, byte[] salt) { 
+        }
+
+        public string DecryptCreditCard(string clearPasswor, byte[] passwordHash, string creditCard)
+        {
+        }
+        public byte[] EncryptCreditCard(string clearPasswor, byte[] passwordHash, string creditCard)
+        {
+        }
+
+    }
+}
---
+++ DSA/DSA-CustomerLogin/TestCustomerLogin/CustomerLogin/CustomersDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerLogin
+{
+    public class CustomersDB
+    {
+        private Dictionary<string, Customer> _customers=new ();
+        public CustomersDB()
+        {
+            
+        }
+        public bool Login(string username, string password) {
+            return false;
+        }
+
+        public string ReadCreditCard(string username, string password)
+        {
+            return string.Empty;
+        }
+
+    }
+}
