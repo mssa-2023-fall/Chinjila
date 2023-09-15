1221307cae147907f73dd880f021f98508f74834
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-09-14 19:19:00 -0500

    Update CustomersDB to use azure table and key vault

--- DSA/DSA-CustomerLogin/TestCustomerLogin/CustomerLogin/Customer.cs
+++ DSA/DSA-CustomerLogin/TestCustomerLogin/CustomerLogin/Customer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Azure;
+using Azure.Data.Tables;
+using Azure.Security.KeyVault.Keys.Cryptography;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -7,25 +10,41 @@ using System.Threading.Tasks;
 
 namespace CustomerLogin
 {
-    public class Customer
+    public class Customer : ITableEntity
     {
         private CryptoHelper _cryptoHelper = new();
-        public Customer(string name,string email, string password, string creditCard)
+        public Customer(string name, string email, string password, string creditCard, CryptographyClient? _cryptographyClient=null )
         {
             Salt = RandomNumberGenerator.GetBytes(64);
             Name = name;
             Email = email;
             PasswordHash = _cryptoHelper.ComputeHash(password, Salt);
-            CreditCardHash = _cryptoHelper.EncryptCreditCard(password,creditCard).Result;
+            if (_cryptographyClient != null)
+            { 
+                CreditCardHash = _cryptographyClient.Encrypt(EncryptionAlgorithm.RsaOaep,Encoding.UTF8.GetBytes(creditCard)).Ciphertext;
+            }
+            else 
+            { 
+                CreditCardHash = _cryptoHelper.EncryptCreditCard(password, creditCard).Result; }
+            
+
+        }
+        public Customer()
+        {
+
         }
 
         public byte[] Salt;
-        public string Name { get; }
-        public string Email { get; }
-        public byte[] PasswordHash { get; }
-        public byte[] CreditCardHash { get; }
-   
+        public string Name;
+        public string Email;
+        public byte[] PasswordHash;
+        public byte[] CreditCardHash;
+
+        public string PartitionKey { get => Email; set => this.Email = value; }
+        public string RowKey { get => Name; set => this.Name = value; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+
     }
-
-
 }
--- DSA/DSA-CustomerLogin/TestCustomerLogin/CustomerLogin/CustomersDB.cs
+++ DSA/DSA-CustomerLogin/TestCustomerLogin/CustomerLogin/CustomersDB.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Azure.Data.Tables;
+using Azure.Security.KeyVault.Keys.Cryptography;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,9 @@ namespace CustomerLogin
     {
         private Dictionary<string, Customer> _customers=new ();
         private CryptoHelper _cryptoHelper= new();
+        private readonly TableClient table;
+        private readonly CryptographyClient key;
+
         public Customer this[string email] => _customers[email];
         public int Count => _customers.Count;
         public CustomersDB()
@@ -22,6 +27,23 @@ namespace CustomerLogin
             _customers.Add(bob.Email, bob);
             _customers.Add(charlie.Email, charlie);
         }
+
+        public CustomersDB(TableClient _table,CryptographyClient _key)
+        {
+            Customer alice = new Customer("Alice", "alice@live.com", "password", "11112222", _key);
+            Customer bob = new Customer("bob", "bob@live.com", "password", "11112222", _key);
+            Customer charlie = new Customer("charlie", "charlie@live.com", "password", "11112222", _key);
+            _table.AddEntity(alice);
+            _table.AddEntity(bob);
+            _table.AddEntity(charlie);
+            var allCustomers = _table.Query<Customer>();
+            foreach (var customer in allCustomers)
+            {
+                _customers.Add(customer.Email, customer);
+            }
+            table = _table;
+            key = _key;
+        }
         public bool Login(string username, string password) {
             if (!_customers.ContainsKey(username)) { return false; }
             else { 
@@ -31,16 +53,31 @@ namespace CustomerLogin
 
         public string ReadCreditCard(string username, string password)
         {
-            if (!_customers.ContainsKey(username)) { throw new Exception("Read Failed"); }
-            var creditCardPlainText=string.Empty;
-            try
+            string creditCardPlainText=string.Empty;
+            if (table != null && key != null)
             {
-                creditCardPlainText = _cryptoHelper.DecryptCreditCard(password, _customers[username].CreditCardHash).Result;
-            }
-            catch (Exception)
-            {
+                try {
+                    creditCardPlainText= Encoding.UTF8.GetString(key.Decrypt(EncryptionAlgorithm.RsaOaep, this._customers[username].CreditCardHash).Plaintext);
+                }
+                catch (Exception)
+                {
 
-                throw new Exception("Read Failed"); ;
+                    throw new Exception("Read Failed"); ;
+                }
+            }
+            else
+            {
+                if (!_customers.ContainsKey(username)) { throw new Exception("Read Failed"); }
+                creditCardPlainText = string.Empty;
+                try
+                {
+                    creditCardPlainText = _cryptoHelper.DecryptCreditCard(password, _customers[username].CreditCardHash).Result;
+                }
+                catch (Exception)
+                {
+
+                    throw new Exception("Read Failed"); ;
+                } 
             }
                 return creditCardPlainText;
         }
