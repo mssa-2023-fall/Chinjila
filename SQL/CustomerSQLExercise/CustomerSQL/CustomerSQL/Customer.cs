4f40b2c2e3faca5cae0abd85a93503f7051c6f42
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-09-26 14:23:47 -0500

    Added SQL C# program

---
+++ SQL/CustomerSQLExercise/CustomerSQL/CustomerSQL/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerSQL
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
+
+    }
+}
