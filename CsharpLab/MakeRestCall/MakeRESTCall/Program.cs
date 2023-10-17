1763c2b40b375ae301f85c2e1638674a7f780160
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-10-17 13:11:47 -0500

    Refactor return type from Task to task<List>

--- CsharpLab/MakeRestCall/MakeRESTCall/Program.cs
+++ CsharpLab/MakeRestCall/MakeRESTCall/Program.cs
@@ -10,13 +10,14 @@ client.DefaultRequestHeaders.Accept.Add(
 
 client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-await ProcessRepositoriesAsync(client);
+var repositories = await ProcessRepositoriesAsync(client);
 
-static async Task ProcessRepositoriesAsync(HttpClient client)
+foreach (var repo in repositories)
+    Console.WriteLine(repo.Name);
+static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
 {
     await using Stream stream = await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
     var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
 
-    foreach (var repo in repositories ?? Enumerable.Empty<Repository>())
-        Console.WriteLine(repo.Name);
+    return repositories ?? new();
 }
