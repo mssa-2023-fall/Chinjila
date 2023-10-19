847fc63a3ac8725eb07ee97c04de59639c4170f6
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-10-19 12:49:12 -0500

    dynamic file parser

---
+++ CsharpLab/DynamicClass/DynamicFileReader/DynamicFileReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using DynamicFileReader;
+
+dynamic rFile = new ReadOnlyFile(@"..\..\..\TextFile1.txt");
+foreach (string line in rFile.Customer)
+{
+    Console.WriteLine(line);
+}
+Console.WriteLine("----------------------------");
+foreach (string line in rFile.Supplier(StringSearchOption.StartsWith, true))
+{
+    Console.WriteLine(line);
+}
