21f369bc92e3113b5a36e43c35e216527aef9913
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-10-30 10:13:29 -0500

    Basic addition using Context.Request.Path Property

--- ASPDotNet/Middleware/BasicMiddleware/BasicMiddleware/BasicMiddleware/Program.cs
+++ ASPDotNet/Middleware/BasicMiddleware/BasicMiddleware/BasicMiddleware/Program.cs
@@ -3,10 +3,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 
+//app.Run(async context =>
+//{
+//    await context.Response.WriteAsync("Hello world!");
+//});
 app.Run(async context =>
 {
-    await context.Response.WriteAsync("Hello world!");
+    var path = context.Request.Path;
+    if (path.Value?.Split('/').Length > 2)
+        if (int.TryParse(path.Value?.Split('/')[1], out int a) & int.TryParse(path.Value?.Split('/')[2], out int b))
+        { await context.Response.WriteAsync($"{a + b}");
+            return;
+        }
+  
+await context.Response.WriteAsync("Hello world!");
 });
-
 
 app.Run();
