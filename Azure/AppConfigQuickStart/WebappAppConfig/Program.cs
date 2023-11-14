a88a248d9027540c9bc3c0b38ab7f180a1b4fff4
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-11-14 10:28:05 -0600

    Feature Gate Sample

--- Azure/AppConfigQuickStart/WebappAppConfig/Program.cs
+++ Azure/AppConfigQuickStart/WebappAppConfig/Program.cs
@@ -1,5 +1,7 @@
 using Azure.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.FeatureManagement;
 using Microsoft.Identity.Client;
 using TestAppConfig;
 
@@ -7,16 +9,37 @@ var builder = WebApplication.CreateBuilder(args);
 string connectionString = builder.Configuration.GetConnectionString("AppConfig");
 
 // Load configuration from Azure App Configuration
-builder.Configuration.AddAzureAppConfiguration(connectionString);
+//builder.Configuration.AddAzureAppConfiguration(connectionString);
 //builder.Configuration.AddAzureAppConfiguration(option =>
 //{
 //    option.Connect(new Uri("https://vcappconfig.azconfig.io"),new ManagedIdentityCredential());
 //});
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    options.Connect(connectionString)
+           // Load all keys that start with `TestApp:` and have no label
+           .Select("TestApp:*", LabelFilter.Null)
+           // Configure to reload configuration if the registered sentinel key is modified
+           .ConfigureRefresh(refreshOptions =>
+                refreshOptions.Register("TestApp:Settings:Sentinel", refreshAll: true));
 
+    // Load all feature flags with no label
+    options.UseFeatureFlags(featureFlagOptions =>
+    {
+        featureFlagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(5);
+    });
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+// Add Azure App Configuration middleware to the container of services.
+builder.Services.AddAzureAppConfiguration();
+
+// Add feature management to the container of services.
+builder.Services.AddFeatureManagement();
+
 builder.Services.Configure<Settings>(builder.Configuration.GetSection("TestApp:Settings"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +52,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+
+app.UseAzureAppConfiguration();
 
 app.UseRouting();
 
